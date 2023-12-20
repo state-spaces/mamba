bd011a1f1988f60f58f31d5d38013d6681e278d0
Tri Dao <tridpq@gmail.com>
2023-12-19 21:21:37 -0800

    Change MambaConfig to a dataclass

--- mamba_ssm/models/config_mamba.py
+++ mamba_ssm/models/config_mamba.py
@@ -1,20 +1,14 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
 class MambaConfig:
-    def __init__(
-        self,
-        d_model: 2560,
-        n_layer: 64,
-        vocab_size: 50277,
-        ssm_cfg: {},
-        rms_norm: True,
-        residual_in_fp32: True,
-        fused_add_norm: True,
-        pad_vocab_size_multiple: 8
-    ):
-        self.d_model = d_model
-        self.n_layer = n_layer
-        self.vocab_size = vocab_size
-        self.ssm_cfg = ssm_cfg
-        self.rms_norm = rms_norm
-        self.residual_in_fp32 = residual_in_fp32
-        self.fused_add_norm = fused_add_norm
-        self.pad_vocab_size_multiple = pad_vocab_size_multiple
+
+    d_model: int = 2560
+    n_layer: int = 64
+    vocab_size: int = 50277
+    ssm_cfg: dict = field(default_factory=dict)
+    rms_norm: bool = True
+    residual_in_fp32: bool = True
+    fused_add_norm: bool = True
+    pad_vocab_size_multiple: int = 8
