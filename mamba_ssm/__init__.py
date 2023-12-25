fb7b5310fa865dbd62aa059b1e26f2b431363e2a
Tri Dao <tridpq@gmail.com>
2023-12-25 11:00:13 -0800

    Bump to v1.1.1

--- mamba_ssm/__init__.py
+++ mamba_ssm/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 from mamba_ssm.modules.mamba_simple import Mamba
