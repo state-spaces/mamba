28b06f1ece3a304c8ab4fca8f756761b6d2f0f15
Erik Garrison <erik.garrison@gmail.com>
2024-01-05 17:30:13 -0600

    in time

--- train/train.py
+++ train/train.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 import numpy as np
 import shutil
 import json
+import time
 from torch.utils.data import Dataset, DataLoader, random_split
 from torch.nn.utils.rnn import pad_sequence
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
