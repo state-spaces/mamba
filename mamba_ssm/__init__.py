009bec5ee37f586844a3fc89c040a9c1a9d8badf
Tri Dao <tridpq@gmail.com>
2024-01-31 18:51:50 -0800

    [CI] Fix CUDA 12.2 compilation

--- mamba_ssm/__init__.py
+++ mamba_ssm/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.3"
+__version__ = "1.1.3.post1"
 
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 from mamba_ssm.modules.mamba_simple import Mamba
