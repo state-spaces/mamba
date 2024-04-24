37f67eb186cd702af1f6f1d96141de635718211d
Eden Lumbroso <edenlum@gmail.com>
2024-04-24 14:50:05 +0300

    adding d_State

--- induction_head_run.py
+++ induction_head_run.py
@@ -12,9 +12,9 @@ from simple_mamba.mamba_lm import MambaLM, MambaLMConfig
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
 # Assumptions: 'model', 'dataloader', 'device', 'optim' (optimizer) are already defined
 def train(config, model, data_loader, optimizer):
-
     # Setup tqdm for the outer loop
     pbar = tqdm(total=config.epochs, desc="Epoch Progress", position=0)
 
@@ -24,12 +24,12 @@ def train(config, model, data_loader, optimizer):
         for data, labels in data_loader:
             data = data.to(device).long()  # Ensure data is on the correct device and dtype
             labels = labels.to(device).long()  # Ensure labels are on the correct device and converted to long        
-            
+
             # Forward pass
             logits = model(data)  # [batch_size, seq_len, cat_num]
-            
+
             # Compute loss
-            loss = F.cross_entropy(logits[:, -config.induction_len:, :].reshape(-1, config.n_categories), 
+            loss = F.cross_entropy(logits[:, -config.induction_len:, :].reshape(-1, config.n_categories),
                                    labels[:, -config.induction_len:].reshape(-1))
 
             # Backpropagation
@@ -38,11 +38,11 @@ def train(config, model, data_loader, optimizer):
             optimizer.step()
 
             avg_loss += loss.item()
-        
+
         avg_loss /= len(data_loader)
         wandb.log({"epoch": epoch,
-                    "loss": avg_loss})
-        
+                   "loss": avg_loss})
+
         if config.stop_on_loss and avg_loss < config.stop_on_loss:
             break
     pbar.close()
@@ -52,6 +52,7 @@ def train(config, model, data_loader, optimizer):
 class Config:
     ssm_type: str
     d_model: int
+    d_state: int
     n_layers: int
     n_categories: int
     seq_len: int
@@ -65,6 +66,7 @@ class Config:
     seed: int
     comment: str
 
+
 def experiments(kwargs):
     # Extract argument names and their corresponding value lists
     arg_names = kwargs.keys()
@@ -75,48 +77,53 @@ def experiments(kwargs):
         # Yield a dictionary mapping argument names to values
         yield dict(zip(arg_names, values))
 
+
 def run_experiment(config):
     torch.manual_seed(config.seed)
     np.random.seed(config.seed)
     mamba_config = MambaLMConfig(
-                           ssm_type=config.ssm_type,
-                           d_model=config.d_model, 
-                           n_layers=config.n_layers, 
-                           vocab_size=config.n_categories, 
-                           pad_vocab_size_multiple=config.n_categories)
-    
-    dataset = InductionHead(config.epoch_size, 
-                            config.seq_len, 
+        ssm_type=config.ssm_type,
+        d_model=config.d_model,
+        n_layers=config.n_layers,
+        vocab_size=config.n_categories,
+        pad_vocab_size_multiple=config.n_categories,
+        d_state=config.d_state)
+
+    dataset = InductionHead(config.epoch_size,
+                            config.seq_len,
                             config.n_categories,
                             config.num_triggers,
                             config.induction_len)
-    data_loader = torch.utils.data.DataLoader(dataset, 
-                                              batch_size=config.batch_size, 
+    data_loader = torch.utils.data.DataLoader(dataset,
+                                              batch_size=config.batch_size,
                                               shuffle=True)
     model = MambaLM(mamba_config).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=config.lr)    
+    optimizer = optim.Adam(model.parameters(), lr=config.lr)
     train(config, model, data_loader, optimizer)
+
 
 def name(config):
     # short name for display on wandb
     return f"Induction-{config.ssm_type}-seq_len{config.seq_len}-ind_len{config.induction_len}"
 
+
 def main():
     for i, config in enumerate(experiments({
-            "ssm_type":              ["S4D-Complex", "conv", "S4D-Real", "S6-Real"],
-            "d_model":               [64],
-            "n_layers":              [2],
-            "n_categories":          [16],
-            "seq_len":               [256],
-            "induction_len":         [2, 4, 8],
-            "num_triggers":          [1],
-            "batch_size":            [8],
-            "epochs":                [1600],
-            "epoch_size":            [256],
-            "lr":                    [1e-3],
-            "stop_on_loss":          [0.01],
-            "seed":                  [1],   
-            })):
+        "ssm_type": ["S4D-Real", "S4D-Complex", "S6-Real"],
+        "d_model": [64],
+        "d_state": [1],
+        "n_layers": [2],
+        "n_categories": [16],
+        "seq_len": [256],
+        "induction_len": [2, 4, 8],
+        "num_triggers": [1],
+        "batch_size": [8],
+        "epochs": [1600],
+        "epoch_size": [256],
+        "lr": [1e-3],
+        "stop_on_loss": [0.01],
+        "seed": [1],
+    })):
         config.update({"comment": ""})
         exp_name = name(Config(**config))
         wandb.init(
@@ -129,5 +136,6 @@ def main():
         wandb.finish()
     print('Finished Training')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
--- simple_mamba/mamba.py
+++ simple_mamba/mamba.py
@@ -28,32 +28,34 @@ See Figure 3 of the paper (page 8) for a visual representation of a MambaBlock.
 
 """
 
+
 @dataclass
 class MambaConfig:
     ssm_type: str
-    d_model: int # D
+    d_model: int  #  D
     n_layers: int
     dt_rank: Union[int, str] = 'auto'
-    d_state: int = 16 # N in paper/comments
-    expand_factor: int = 2 # E in paper/comments
+    d_state: int = 16  #  N in paper/comments
+    expand_factor: int = 2  #  E in paper/comments
     d_conv: int = 4
 
     dt_min: float = 0.001
     dt_max: float = 0.1
-    dt_init: str = "random" # "random" or "constant"
+    dt_init: str = "random"  #  "random" or "constant"
     dt_scale: float = 1.0
     dt_init_floor = 1e-4
 
     bias: bool = False
     conv_bias: bool = True
 
-    pscan: bool = True # use parallel scan mode or sequential mode when training
+    pscan: bool = True  #  use parallel scan mode or sequential mode when training
 
     def __post_init__(self):
-        self.d_inner = self.expand_factor * self.d_model # E*D = ED in comments
+        self.d_inner = self.expand_factor * self.d_model  # E*D = ED in comments
 
         if self.dt_rank == 'auto':
             self.dt_rank = math.ceil(self.d_model / 16)
+
 
 class Mamba(nn.Module):
     def __init__(self, config: MambaConfig):
@@ -62,31 +64,32 @@ class Mamba(nn.Module):
         self.config = config
 
         self.layers = nn.ModuleList([ResidualBlock(config) for _ in range(config.n_layers)])
-        #self.norm_f = RMSNorm(config.d_model)
+        # self.norm_f = RMSNorm(config.d_model)
 
     def forward(self, x):
-        # x : (B, L, D)
+        #  x : (B, L, D)
 
-        # y : (B, L, D)
+        #  y : (B, L, D)
 
         for layer in self.layers:
             x = layer(x)
 
-        #x = self.norm_f(x)
+        # x = self.norm_f(x)
 
         return x
-    
-    def step(self, x, caches):
-        # x : (B, L, D)
-        # caches : [cache(layer) for all layers], cache : (h, inputs)
 
-        # y : (B, L, D)
-        # caches : [cache(layer) for all layers], cache : (h, inputs)
+    def step(self, x, caches):
+        #  x : (B, L, D)
+        #  caches : [cache(layer) for all layers], cache : (h, inputs)
+
+        #  y : (B, L, D)
+        #  caches : [cache(layer) for all layers], cache : (h, inputs)
 
         for i, layer in enumerate(self.layers):
             x, caches[i] = layer.step(x, caches[i])
 
         return x, caches
+
 
 class ResidualBlock(nn.Module):
     def __init__(self, config: MambaConfig):
@@ -96,25 +99,26 @@ class ResidualBlock(nn.Module):
         self.norm = RMSNorm(config.d_model)
 
     def forward(self, x):
-        # x : (B, L, D)
+        #  x : (B, L, D)
 
-        # output : (B, L, D)
+        #  output : (B, L, D)
 
         output = self.mixer(self.norm(x)) + x
         return output
-    
-    def step(self, x, cache):
-        # x : (B, D)
-        # cache : (h, inputs)
-                # h : (B, ED, N)
-                # inputs: (B, ED, d_conv-1)
 
-        # output : (B, D)
-        # cache : (h, inputs)
+    def step(self, x, cache):
+        #  x : (B, D)
+        #  cache : (h, inputs)
+        # h : (B, ED, N)
+        #  inputs: (B, ED, d_conv-1)
+
+        #  output : (B, D)
+        #  cache : (h, inputs)
 
         output, cache = self.mixer.step(self.norm(x), cache)
         output = output + x
         return output, cache
+
 
 class MambaBlock(nn.Module):
     def __init__(self, config: MambaConfig):
@@ -122,109 +126,113 @@ class MambaBlock(nn.Module):
 
         self.config = config
 
-        # projects block input from D to 2*ED (two branches)
+        #  projects block input from D to 2*ED (two branches)
         self.in_proj = nn.Linear(config.d_model, 2 * config.d_inner, bias=config.bias)
 
-        self.conv1d = nn.Conv1d(in_channels=config.d_inner, out_channels=config.d_inner, 
-                              kernel_size=config.d_conv, bias=config.conv_bias, 
-                              groups=config.d_inner,
-                              padding=config.d_conv - 1)
-        
+        self.conv1d = nn.Conv1d(in_channels=config.d_inner, out_channels=config.d_inner,
+                                kernel_size=config.d_conv, bias=config.conv_bias,
+                                groups=config.d_inner,
+                                padding=config.d_conv - 1)
+
         if config.ssm_type == "S6-Real":
-            # projects x to input-dependent Δ, B, C
+            #  projects x to input-dependent Δ, B, C
             self.x_proj = nn.Linear(config.d_inner, config.dt_rank + 2 * config.d_state, bias=False)
 
-            # projects Δ from dt_rank to d_inner
+            #  projects Δ from dt_rank to d_inner
             self.dt_proj = nn.Linear(config.dt_rank, config.d_inner, bias=True)
 
-            # dt initialization
-            # dt weights
-            dt_init_std = config.dt_rank**-0.5 * config.dt_scale
+            #  dt initialization
+            #  dt weights
+            dt_init_std = config.dt_rank ** -0.5 * config.dt_scale
             if config.dt_init == "constant":
                 nn.init.constant_(self.dt_proj.weight, dt_init_std)
             elif config.dt_init == "random":
                 nn.init.uniform_(self.dt_proj.weight, -dt_init_std, dt_init_std)
             else:
                 raise NotImplementedError
-            
+
             # dt bias
             dt = torch.exp(
-                torch.rand(config.d_inner) * (math.log(config.dt_max) - math.log(config.dt_min)) + math.log(config.dt_min)
+                torch.rand(config.d_inner) * (math.log(config.dt_max) - math.log(config.dt_min)) + math.log(
+                    config.dt_min)
             ).clamp(min=config.dt_init_floor)
-            inv_dt = dt + torch.log(-torch.expm1(-dt)) # inverse of softplus: https://github.com/pytorch/pytorch/issues/72759
+            inv_dt = dt + torch.log(
+                -torch.expm1(-dt))  #  inverse of softplus: https://github.com/pytorch/pytorch/issues/72759
             with torch.no_grad():
                 self.dt_proj.bias.copy_(inv_dt)
-            #self.dt_proj.bias._no_reinit = True # initialization would set all Linear.bias to zero, need to mark this one as _no_reinit
-            # todo : explain why removed
+            # self.dt_proj.bias._no_reinit = True # initialization would set all Linear.bias to zero, need to mark this one as _no_reinit
+            #  todo : explain why removed
 
             # S4D real initialization
             A = torch.arange(1, config.d_state + 1, dtype=torch.float32).repeat(config.d_inner, 1)
-            self.A_log = nn.Parameter(torch.log(A)) # why store A in log ? to keep A < 0 (cf -torch.exp(...)) ? for gradient stability ?
+            self.A_log = nn.Parameter(
+                torch.log(A))  # why store A in log ? to keep A < 0 (cf -torch.exp(...)) ? for gradient stability ?
             self.D = nn.Parameter(torch.ones(config.d_inner))
 
         elif config.ssm_type == "S4D-Complex":
             self.ssm_kernel = FFTConv(d_model=config.d_inner,
                                       d_state=config.d_state,
-                                      activation='id', 
+                                      activation='id',
                                       transposed=False,
                                       mode='s4d',
                                       is_real=False)
         elif config.ssm_type == "S4D-Real":
             self.ssm_kernel = FFTConv(d_model=config.d_inner,
                                       d_state=config.d_state,
-                                      activation='id', 
+                                      activation='id',
                                       transposed=False,
                                       mode='s4d',
                                       is_real=True)
         elif config.ssm_type == "conv":
-            self.ssm_kernel = nn.Conv1d(in_channels=config.d_inner, out_channels=config.d_inner, 
-                              kernel_size=config.d_state, bias=False, 
-                              groups=config.d_inner,
-                              padding=config.d_state - 1)
+            self.ssm_kernel = nn.Conv1d(in_channels=config.d_inner, out_channels=config.d_inner,
+                                        kernel_size=config.d_state, bias=False,
+                                        groups=config.d_inner,
+                                        padding=config.d_state - 1)
         else:
             raise NotImplementedError
 
-        # projects block output from ED back to D
+        #  projects block output from ED back to D
         self.out_proj = nn.Linear(config.d_inner, config.d_model, bias=config.bias)
 
     def forward(self, x):
-        # x : (B, L, D)
-        
+        #  x : (B, L, D)
+
         # y : (B, L, D)
 
         _, L, _ = x.shape
 
-        xz = self.in_proj(x) # (B, L, 2*ED)
-        x, z = xz.chunk(2, dim=-1) # (B, L, ED), (B, L, ED)
+        xz = self.in_proj(x)  # (B, L, 2*ED)
+        x, z = xz.chunk(2, dim=-1)  #  (B, L, ED), (B, L, ED)
 
-        # x branch
-        x = x.transpose(1, 2) # (B, ED, L)
-        x = self.conv1d(x)[:, :, :L] # depthwise convolution over time, with a short filter
-        x = x.transpose(1, 2) # (B, L, ED)
+        #  x branch
+        x = x.transpose(1, 2)  #  (B, ED, L)
+        x = self.conv1d(x)[:, :, :L]  #  depthwise convolution over time, with a short filter
+        x = x.transpose(1, 2)  #  (B, L, ED)
 
         x = F.silu(x)
         y = self.ssm(x)
 
-        # z branch
+        #  z branch
         z = F.silu(z)
         output = y * z
-        output = self.out_proj(output) # (B, L, D)
+        output = self.out_proj(output)  #  (B, L, D)
 
         return output
-    
+
     def ssm(self, x):
-        # x : (B, L, ED)
+        #  x : (B, L, ED)
 
-        # y : (B, L, ED)
+        #  y : (B, L, ED)
         if self.config.ssm_type == "S6-Real":
-            A = -torch.exp(self.A_log.float()) # (ED, N)
+            A = -torch.exp(self.A_log.float())  # (ED, N)
             D = self.D.float()
-            # TODO remove .float()
+            #  TODO remove .float()
 
-            deltaBC = self.x_proj(x) # (B, L, dt_rank+2*N)
+            deltaBC = self.x_proj(x)  #  (B, L, dt_rank+2*N)
 
-            delta, B, C = torch.split(deltaBC, [self.config.dt_rank, self.config.d_state, self.config.d_state], dim=-1) # (B, L, dt_rank), (B, L, N), (B, L, N)
-            delta = F.softplus(self.dt_proj(delta)) # (B, L, ED)
+            delta, B, C = torch.split(deltaBC, [self.config.dt_rank, self.config.d_state, self.config.d_state],
+                                      dim=-1)  #  (B, L, dt_rank), (B, L, N), (B, L, N)
+            delta = F.softplus(self.dt_proj(delta))  #  (B, L, ED)
 
             if self.config.pscan:
                 y = self.selective_scan(x, delta, A, B, C, D)
@@ -241,63 +249,63 @@ class MambaBlock(nn.Module):
             return out.transpose(-1, -2)
         else:
             raise NotImplementedError
-    
+
     def selective_scan(self, x, delta, A, B, C, D):
-        # x : (B, L, ED)
-        # Δ : (B, L, ED)
-        # A : (ED, N)
-        # B : (B, L, N)
-        # C : (B, L, N)
-        # D : (ED)
+        #  x : (B, L, ED)
+        #  Δ : (B, L, ED)
+        #  A : (ED, N)
+        #  B : (B, L, N)
+        #  C : (B, L, N)
+        #  D : (ED)
 
-        # y : (B, L, ED)
+        #  y : (B, L, ED)
 
-        deltaA = torch.exp(delta.unsqueeze(-1) * A) # (B, L, ED, N)
-        deltaB = delta.unsqueeze(-1) * B.unsqueeze(2) # (B, L, ED, N)
+        deltaA = torch.exp(delta.unsqueeze(-1) * A)  #  (B, L, ED, N)
+        deltaB = delta.unsqueeze(-1) * B.unsqueeze(2)  #  (B, L, ED, N)
 
-        BX = deltaB * (x.unsqueeze(-1)) # (B, L, ED, N)
-        
+        BX = deltaB * (x.unsqueeze(-1))  #  (B, L, ED, N)
+
         hs = pscan(deltaA, BX)
 
-        y = (hs @ C.unsqueeze(-1)).squeeze(3) # (B, L, ED, N) @ (B, L, N, 1) -> (B, L, ED, 1)
+        y = (hs @ C.unsqueeze(-1)).squeeze(3)  #  (B, L, ED, N) @ (B, L, N, 1) -> (B, L, ED, 1)
 
         y = y + D * x
 
         return y
-    
-    def selective_scan_seq(self, x, delta, A, B, C, D):
-        # x : (B, L, ED)
-        # Δ : (B, L, ED)
-        # A : (ED, N)
-        # B : (B, L, N)
-        # C : (B, L, N)
-        # D : (ED)
 
-        # y : (B, L, ED)
+    def selective_scan_seq(self, x, delta, A, B, C, D):
+        #  x : (B, L, ED)
+        #  Δ : (B, L, ED)
+        #  A : (ED, N)
+        #  B : (B, L, N)
+        #  C : (B, L, N)
+        #  D : (ED)
+
+        #  y : (B, L, ED)
 
         _, L, _ = x.shape
 
-        deltaA = torch.exp(delta.unsqueeze(-1) * A) # (B, L, ED, N)
-        deltaB = delta.unsqueeze(-1) * B.unsqueeze(2) # (B, L, ED, N)
+        deltaA = torch.exp(delta.unsqueeze(-1) * A)  #  (B, L, ED, N)
+        deltaB = delta.unsqueeze(-1) * B.unsqueeze(2)  #  (B, L, ED, N)
 
-        BX = deltaB * (x.unsqueeze(-1)) # (B, L, ED, N)
+        BX = deltaB * (x.unsqueeze(-1))  #  (B, L, ED, N)
 
-        h = torch.zeros(x.size(0), self.config.d_inner, self.config.d_state, device=deltaA.device) # (B, ED, N)
+        h = torch.zeros(x.size(0), self.config.d_inner, self.config.d_state, device=deltaA.device)  #  (B, ED, N)
         hs = []
 
         for t in range(0, L):
             h = deltaA[:, t] * h + BX[:, t]
             hs.append(h)
-            
-        hs = torch.stack(hs, dim=1) # (B, L, ED, N)
 
-        y = (hs @ C.unsqueeze(-1)).squeeze(3) # (B, L, ED, N) @ (B, L, N, 1) -> (B, L, ED, 1)
+        hs = torch.stack(hs, dim=1)  #  (B, L, ED, N)
+
+        y = (hs @ C.unsqueeze(-1)).squeeze(3)  #  (B, L, ED, N) @ (B, L, N, 1) -> (B, L, ED, 1)
 
         y = y + D * x
 
         return y
-    
-    # -------------------------- inference -------------------------- #
+
+    #  -------------------------- inference -------------------------- #
     """
     Concerning auto-regressive inference
 
@@ -318,74 +326,77 @@ class MambaBlock(nn.Module):
 
     As we need one such cache variable per layer, we store a caches object, which is simply a list of cache object. (See mamba_lm.py)
     """
-    
-    def step(self, x, cache):
-        # x : (B, D)
-        # cache : (h, inputs)
-                # h : (B, ED, N)
-                # inputs : (B, ED, d_conv-1)
-        
-        # y : (B, D)
-        # cache : (h, inputs)
-        
-        h, inputs = cache
-        
-        xz = self.in_proj(x) # (B, 2*ED)
-        x, z = xz.chunk(2, dim=1) # (B, ED), (B, ED)
 
-        # x branch
+    def step(self, x, cache):
+        #  x : (B, D)
+        #  cache : (h, inputs)
+        # h : (B, ED, N)
+        #  inputs : (B, ED, d_conv-1)
+
+        #  y : (B, D)
+        #  cache : (h, inputs)
+
+        h, inputs = cache
+
+        xz = self.in_proj(x)  # (B, 2*ED)
+        x, z = xz.chunk(2, dim=1)  #  (B, ED), (B, ED)
+
+        #  x branch
         x_cache = x.unsqueeze(2)
-        x = self.conv1d(torch.cat([inputs, x_cache], dim=2))[:, :, self.config.d_conv-1] # (B, ED)
+        x = self.conv1d(torch.cat([inputs, x_cache], dim=2))[:, :, self.config.d_conv - 1]  #  (B, ED)
 
         x = F.silu(x)
         y, h = self.ssm_step(x, h)
 
-        # z branch
+        #  z branch
         z = F.silu(z)
 
         output = y * z
-        output = self.out_proj(output) # (B, D)
+        output = self.out_proj(output)  #  (B, D)
 
         # prepare cache for next call
-        inputs = torch.cat([inputs[:, :, 1:], x_cache], dim=2) # (B, ED, d_conv-1)
+        inputs = torch.cat([inputs[:, :, 1:], x_cache], dim=2)  #  (B, ED, d_conv-1)
         cache = (h, inputs)
-        
+
         return output, cache
 
     def ssm_step(self, x, h):
-        # x : (B, ED)
-        # h : (B, ED, N)
+        #  x : (B, ED)
+        #  h : (B, ED, N)
 
-        # y : (B, ED)
-        # h : (B, ED, N)
+        #  y : (B, ED)
+        #  h : (B, ED, N)
 
-        A = -torch.exp(self.A_log.float()) # (ED, N) # todo : ne pas le faire tout le temps, puisque c'est indépendant de la timestep
+        A = -torch.exp(
+            self.A_log.float())  # (ED, N) # todo : ne pas le faire tout le temps, puisque c'est indépendant de la timestep
         D = self.D.float()
-        # TODO remove .float()
+        #  TODO remove .float()
 
-        deltaBC = self.x_proj(x) # (B, dt_rank+2*N)
+        deltaBC = self.x_proj(x)  #  (B, dt_rank+2*N)
 
-        delta, B, C = torch.split(deltaBC, [self.config.dt_rank, self.config.d_state, self.config.d_state], dim=-1) # (B, dt_rank), (B, N), (B, N)
-        delta = F.softplus(self.dt_proj(delta)) # (B, ED)
+        delta, B, C = torch.split(deltaBC, [self.config.dt_rank, self.config.d_state, self.config.d_state],
+                                  dim=-1)  #  (B, dt_rank), (B, N), (B, N)
+        delta = F.softplus(self.dt_proj(delta))  #  (B, ED)
 
-        deltaA = torch.exp(delta.unsqueeze(-1) * A) # (B, ED, N)
-        deltaB = delta.unsqueeze(-1) * B.unsqueeze(1) # (B, ED, N)
+        deltaA = torch.exp(delta.unsqueeze(-1) * A)  #  (B, ED, N)
+        deltaB = delta.unsqueeze(-1) * B.unsqueeze(1)  #  (B, ED, N)
 
-        BX = deltaB * (x.unsqueeze(-1)) # (B, ED, N)
+        BX = deltaB * (x.unsqueeze(-1))  #  (B, ED, N)
 
         if h is None:
-            h = torch.zeros(x.size(0), self.config.d_inner, self.config.d_state, device=deltaA.device) # (B, ED, N)
+            h = torch.zeros(x.size(0), self.config.d_inner, self.config.d_state, device=deltaA.device)  #  (B, ED, N)
 
-        h = deltaA * h + BX # (B, ED, N)
+        h = deltaA * h + BX  #  (B, ED, N)
 
-        y = (h @ C.unsqueeze(-1)).squeeze(2) # (B, ED, N) @ (B, N, 1) -> (B, ED, 1)
+        y = (h @ C.unsqueeze(-1)).squeeze(2)  #  (B, ED, N) @ (B, N, 1) -> (B, ED, 1)
 
         y = y + D * x
 
-        # todo : pq h.squeeze(1) ??
+        #  todo : pq h.squeeze(1) ??
         return y, h.squeeze(1)
 
-# taken straight from https://github.com/johnma2006/mamba-minimal/blob/master/model.py
+
+#  taken straight from https://github.com/johnma2006/mamba-minimal/blob/master/model.py
 class RMSNorm(nn.Module):
     def __init__(self, d_model: int, eps: float = 1e-5):
         super().__init__()
