df4edb956c6896b54584d1c4bd7f00d76e49ef59
Stephen Youn <stepyoun@amd.com>
2024-06-07 18:42:05 +0000

    modify mamba triton kernels compatible to triton version >= 3.0.0

---
+++ mamba_ssm/ops/triton/softplus.py
@@ -0,0 +1,17 @@
+import triton
+import triton.language as tl
+from packaging import version
+
+TRITON3 = version.parse(triton.__version__) >= version.parse("3.0.0")
+
+
+if TRITON3:
+    @triton.jit
+    def softplus(dt):
+        dt = tl.where(dt <= 20.0, tl.math.log(tl.math.exp(dt) + 1), dt)
+        return dt
+else:
+    @triton.jit
+    def softplus(dt):
+        dt = tl.where(dt <= 20.0, tl.math.log1p(tl.exp(dt)), dt)
+        return dt
