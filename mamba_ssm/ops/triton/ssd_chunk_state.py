9af5235752a6c49f64472c222e9a5b12121fc990
Stephen Youn <stepyoun@amd.com>
2024-09-24 22:38:10 +0000

    fixing softplus bug with _chunk_cumsum_bwd_kernel() triton kernel

--- mamba_ssm/ops/triton/ssd_chunk_state.py
+++ mamba_ssm/ops/triton/ssd_chunk_state.py
@@ -68,7 +68,7 @@ def _chunk_cumsum_fwd_kernel(
         dt_bias = tl.load(dt_bias_ptr + offs_h * stride_dt_bias_head, mask=offs_h < nheads, other=0.0).to(tl.float32)
         dt += dt_bias[:, None]
     if DT_SOFTPLUS:
-        dt = softplus(dt)
+        dt = tl.where(dt <= 20.0, softplus(dt), dt)
     # As of Triton 2.2.0, tl.clamp is not available yet
     # dt = tl.clamp(dt, dt_min, dt_max)
     dt = tl.minimum(tl.maximum(dt, dt_min), dt_max)
@@ -141,7 +141,7 @@ def _chunk_cumsum_bwd_kernel(
         dt += dt_bias[:, None]
     if DT_SOFTPLUS:
         dt_presoftplus = dt
-        dt = softplus(dt)
+        dt = tl.where(dt <= 20.0, softplus(dt), ddt)
     clamp_mask = (dt < dt_min) | (dt > dt_max)
     # As of Triton 2.2.0, tl.clamp is not available yet
     # dt = tl.clamp(dt, dt_min, dt_max)
