d3e3d526ba69b06c9898eebd1b682c151879207a
Duncan Riach <duncan@nvidia.com>
2025-02-06 17:33:43 -0800

    Fix causal_conv1d xBC stride not multiple of 8 issue

--- mamba_ssm/ops/triton/ssd_combined.py
+++ mamba_ssm/ops/triton/ssd_combined.py
@@ -779,7 +779,7 @@ class MambaSplitConv1dScanCombinedFn(torch.autograd.Function):
         zx0, z, xBC, dt = torch.split(zxbcdt, [2 * d_nonssm, dim, dim + ngroups * dstate * 2, nheads], dim=-1)
         seq_idx = seq_idx.contiguous() if seq_idx is not None else None
         xBC_conv = rearrange(
-            causal_conv1d_cuda.causal_conv1d_fwd(rearrange(xBC, "b s d -> b d s"),
+            causal_conv1d_cuda.causal_conv1d_fwd(rearrange(xBC, "b s d -> b d s").contiguous(),
                                                  conv1d_weight, conv1d_bias, seq_idx, None, None, activation in ["silu", "swish"]),
             "b d s -> b s d"
         )
