175d263c1c0e58b5d5bd2a51eef43c28696396fa
Tri Dao <tridpq@gmail.com>
2026-03-10 15:24:14 +0700

    Don't compile for Pascal and Volta anymore

--- setup.py
+++ setup.py
@@ -172,15 +172,6 @@ if not SKIP_CUDA_BUILD:
                     "Note: make sure nvcc has a supported version by running nvcc -V."
                 )
 
-        if bare_metal_version <= Version("12.9"):
-            cc_flag.append("-gencode")
-            cc_flag.append("arch=compute_53,code=sm_53")
-            cc_flag.append("-gencode")
-            cc_flag.append("arch=compute_62,code=sm_62")
-            cc_flag.append("-gencode")
-            cc_flag.append("arch=compute_70,code=sm_70")
-            cc_flag.append("-gencode")
-            cc_flag.append("arch=compute_72,code=sm_72")
         cc_flag.append("-gencode")
         cc_flag.append("arch=compute_75,code=sm_75")
         cc_flag.append("-gencode")
