3e7baa27a5aabd97e03eb38fcd4c72ec9aa0d209
Dorianhgn <dorianhugonnet1@gmail.com>
2026-05-11 15:40:52 +0200

    chore(ane): export Mamba3ParallelPortable and StatefulMambaParallelHybrid

--- mamba_ane/models/__init__.py
+++ mamba_ane/models/__init__.py
@@ -1,3 +1,7 @@
 from mamba_ane.models.hybrid1d import Hybrid1DBackbone, StatefulMambaHybrid1D
+from mamba_ane.models.hybrid_parallel import ParallelHybridBackbone, StatefulMambaParallelHybrid
 
-__all__ = ["Hybrid1DBackbone", "StatefulMambaHybrid1D"]
+__all__ = [
+    "Hybrid1DBackbone", "StatefulMambaHybrid1D",
+    "ParallelHybridBackbone", "StatefulMambaParallelHybrid",
+]
